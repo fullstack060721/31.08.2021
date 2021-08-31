5ef34bef6b5fd2c41f9c0af4aaa25680ebf6e82b
fullstack060721 <87032460+fullstack060721@users.noreply.github.com>
2021-08-31 20:57:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 8080
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+/*
+const dbURI = 'mongodb+srv://marizza:demo1234@cluster0.nem3y.mongodb.net/node-auth';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+*/
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
